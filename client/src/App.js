b0c5e41b7352107aef328f0c99e5f578951097e2
Corbett <sc145187@umconnect.umt.edu>
2017-08-22 21:33:21 -0600

    init product

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import DataProvider from './containers/DataProvider/DataProvider'
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello!
+        <DataProvider />
       </div>
     );
   }
